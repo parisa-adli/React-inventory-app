edddeba5b1ad5459874ce20f1248c61658d3161b
parisa-adli <parisa.adli1996@gmail.com>
2025-01-05 15:36:41 +0330

    save to localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -40,6 +40,26 @@ function App() {
     });
   };
 
+  useEffect(() => {
+    const savedCategories =
+      JSON.parse(localStorage.getItem("categories")) || [];
+    const savedProducts = JSON.parse(localStorage.getItem("products")) || [];
+    setCategories(savedCategories);
+    setProducts(savedProducts);
+  }, []);
+
+  useEffect(() => {
+    if (categories.length) {
+      localStorage.setItem("categories", JSON.stringify(categories));
+    }
+  }, [categories]);
+
+  useEffect(() => {
+    if (products.length) {
+      localStorage.setItem("products", JSON.stringify(products));
+    }
+  }, [products]);
+
   return (
     <div className="bg-slate-800 min-h-screen">
       <Navbar />
